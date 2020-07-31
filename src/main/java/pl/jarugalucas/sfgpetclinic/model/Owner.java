fa58bd69e27d5b69211cf714378f6326693c5865
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-07-31 15:55:01 +0200

    adding POJOs. Closes #2

---
+++ src/main/java/pl/jarugalucas/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package pl.jarugalucas.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/pl/jarugalucas/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package pl.jarugalucas.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
