0e0339ee6b8d2ec1ae29ce9934b3d0e6a667f344
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-09-05 02:35:29 +0200

    Added PetTypeService and it's implementation. closes #29

---
+++ pet-clinic-data/src/main/java/pl/jarugalucas/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pl.jarugalucas.sfgpetclinic.services;
+
+import pl.jarugalucas.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/pl/jarugalucas/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package pl.jarugalucas.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.jarugalucas.sfgpetclinic.model.PetType;
+import pl.jarugalucas.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
