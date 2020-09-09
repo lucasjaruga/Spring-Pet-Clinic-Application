13a7df793a3747178c02a0883161801bc699f5b6
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-09-09 19:25:27 +0200

    Updated Base Entity with JPA annotations. closes #35

--- pet-clinic-data/src/main/java/pl/jarugalucas/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pl/jarugalucas/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package pl.jarugalucas.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// it sets up this class as a base class for JPA, it means that JPA knows that other classes will inherit from this one
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     //id could be long but Hibernate recommends to use Wrappers, because they can be null
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
